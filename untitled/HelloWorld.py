b15e03b6b1256dee0b7b329778855ea6e53ba6dc
Xie-A2 <31930328+Xie-A2@users.noreply.github.com>
2017-09-14 11:47:56 +0800

    2017-9-14

--- untitled/HelloWorld.py
+++ untitled/HelloWorld.py
@@ -14,21 +14,21 @@ import sys
 
 
 # 正则表达式获取文件ID
-def getHtml(url):
-    page = urllib.urlopen(url)
-    html = page.read()
-    return html
-
-
-def getnub(html):
-    reg = r'href=\'/page/\d\d-\d\d-\d\d/(.+?).html'
-    imgre = re.compile(reg)
-    imglist = re.findall(imgre, html)
-    return imglist
-
-
-html = getHtml("http://www.12999.com/html2/10864.html")
-print getnub(html)
+# def getHtml(url):
+#     page = urllib.urlopen(url)
+#     html = page.read()
+#     return html
+#
+#
+# def getnub(html):
+#     reg = r'href=\'/page/\d\d-\d\d-\d\d/(.+?).html'
+#     imgre = re.compile(reg)
+#     imglist = re.findall(imgre, html)
+#     return imglist
+#
+#
+# html = getHtml("http://www.12999.com/html2/10864.html")
+# print getnub(html)
 
 #测试
 # def getHtml(url):
